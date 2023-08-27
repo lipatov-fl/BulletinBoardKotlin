dfa0b569b6262937a095337f552d214cbae0a56d
lipatov-fl <lipatov-fl@yandex.ru>
2023-08-27 23:54:08 +0400

    Создал функции для регистрации

--- app/src/main/java/com/lipatovfl/bulletinboardkotlin/MainActivity.kt
+++ app/src/main/java/com/lipatovfl/bulletinboardkotlin/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
+import com.google.firebase.auth.FirebaseAuth
 import com.lipatovfl.bulletinboardkotlin.databinding.ActivityMainBinding
 import com.lipatovfl.bulletinboardkotlin.dialogHelper.DialogConst
 import com.lipatovfl.bulletinboardkotlin.dialogHelper.DialogHelper
@@ -14,6 +15,7 @@ import com.lipatovfl.bulletinboardkotlin.dialogHelper.DialogHelper
 class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
     lateinit var bindingClass: ActivityMainBinding
     private val dialogHelper = DialogHelper(this)
+    val mainAuth = FirebaseAuth.getInstance() // Иницилизируем наш обьект mainAuth
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
---
+++ app/src/main/java/com/lipatovfl/bulletinboardkotlin/account_helper/AccountHelper.kt
@@ -0,0 +1,44 @@
+package com.lipatovfl.bulletinboardkotlin.account_helper
+
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.lipatovfl.bulletinboardkotlin.MainActivity
+import com.lipatovfl.bulletinboardkotlin.R
+
+class AccountHelper(private val act: MainActivity) {
+    // Функция для регистрации
+    fun sugnUpWithEmail(email: String, password: String) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            act.mainAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->                    // обьект, который несет в себе информацию, зареган или нет
+                    if (task.isSuccessful) {
+
+                    } else {
+                        Toast.makeText(
+                            act,
+                            act.resources.getString(R.string.sign_up_error),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    private fun sendEmailVerification(user: FirebaseUser) {
+        user.sendEmailVerification().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(
+                    act,
+                    act.resources.getString(R.string.sign_up_error),
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    act,
+                    act.resources.getString(R.string.sign_up_error),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+}
